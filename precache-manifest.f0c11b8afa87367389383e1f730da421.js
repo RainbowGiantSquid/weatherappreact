b3616dac889345572473a3608b677783a7ddc7d2
Dagmara <dagmarakukla@gmail.com>
2019-05-18 12:10:07 +0100

    Updates

---
+++ precache-manifest.f0c11b8afa87367389383e1f730da421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf09a090c914f52d250eca30885e792",
+    "url": "/weatheappreact/index.html"
+  },
+  {
+    "revision": "24f4b885c11d50f37151",
+    "url": "/weatheappreact/static/css/main.2aac2ae6.chunk.css"
+  },
+  {
+    "revision": "246f25f21f6cebc39b26",
+    "url": "/weatheappreact/static/js/2.599af31d.chunk.js"
+  },
+  {
+    "revision": "24f4b885c11d50f37151",
+    "url": "/weatheappreact/static/js/main.a094ef89.chunk.js"
+  },
+  {
+    "revision": "23ff8a0decbd1c118c0d",
+    "url": "/weatheappreact/static/js/runtime~main.92541fd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weatheappreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
